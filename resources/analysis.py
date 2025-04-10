30f9e2ba54a179f561c8f6c603e6fb99a36cad8a
Alexandr <mikhalchenkov22@gmail.com>
2025-04-10 09:22:48 +0300

    dep

--- resources/analysis.py
+++ resources/analysis.py
@@ -8,9 +8,11 @@ class AnalysisResource(Resource):
     @staticmethod
     def get():
         data = request.args.get("data")
+        print("yoy", data)
         if data is None:
             abort(404, message="Данные являются объектом None")
         try:
+            print("here")
             return generate_analysis(data)
         except Exception as e:
             abort(404, message=f"Ошибка при генерации: {e}")
